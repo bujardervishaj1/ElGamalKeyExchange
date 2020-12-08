856600ad18c9974cc9ab560f1e0f5397d4eb8236
Jon Klinaku <jon.klinaku@student.uni-pr.edu>
2020-12-08 21:29:04 +0100

    Created new project

---
+++ ElGamalExampleKeyExchange/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElGamalExampleKeyExchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
