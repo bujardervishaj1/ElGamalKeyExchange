7d3c5ad9171f9275ea03d8099e4c917351ddfd15
Jon Klinaku <jon.klinaku@student.uni-pr.edu>
2020-12-08 21:30:41 +0100

    Working example :)

--- ElGamalExampleKeyExchange/Program.cs
+++ ElGamalExampleKeyExchange/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ElGamalKeyExchange;
+using System;
+using System.Security.Cryptography;
 
 namespace ElGamalExampleKeyExchange
 {
@@ -6,7 +8,29 @@ namespace ElGamalExampleKeyExchange
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rijndael x_rijndael = Rijndael.Create();
+            x_rijndael.KeySize = 128;
+
+            byte[] x_session_key = x_rijndael.Key;
+
+            var x_elgamal = new ElGamalManaged();
+
+
+            ElGamalOAEPKeyExchangeFormatter x_formatter = new ElGamalOAEPKeyExchangeFormatter();
+
+            x_formatter.SetKey(x_elgamal);
+
+            byte[] x_exchange_data = x_formatter.CreateKeyExchange(x_session_key);
+
+            foreach (byte b in x_exchange_data)
+            {
+                Console.Write("{0:X2} ", b);
+            }
+
+            ElGamalOAEPKeyExchangeDeformatter x_deformatter = new ElGamalOAEPKeyExchangeDeformatter();
+            x_deformatter.SetKey(x_elgamal);
+
+            byte[] x_session_key_deformatted = x_deformatter.DecryptKeyExchange(x_exchange_data);
         }
     }
 }
